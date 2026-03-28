82b44566eb191ae8da5fc36ed0c32e058e290fb5
Manoj <manoj@example.com>
2026-03-28 10:52:05 +0530

    Reversed all steps: Removed the 4-step extension installation guide and restored initial layout

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,7 +5,6 @@ import FeaturesSection from "@/components/FeaturesSection";
 import HowItWorks from "@/components/HowItWorks";
 import InteractiveDemo from "@/components/InteractiveDemo";
 import DownloadSection from "@/components/DownloadSection";
-import ExtensionGuide from "@/components/ExtensionGuide";
 import IDEDownloadSection from "@/components/IDEDownloadSection";
 import Footer from "@/components/Footer";
 
@@ -18,7 +17,6 @@ const Index = () => (
     <HowItWorks />
     <InteractiveDemo />
     <DownloadSection />
-    <ExtensionGuide />
     <IDEDownloadSection />
     <Footer />
   </div>
